1788833cd5c957ee65f21699eb7931257d0f3841
Jen Homann <jenhomann@Jens-MBP.igs2.com>
2017-12-06 15:49:55 -0600

    builds basic board and sets up button pressed action for all squares

--- tic-tac-toe/ViewController.swift
+++ tic-tac-toe/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPresed(_ sender: UIButton) {
+        
+        print(sender.tag)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
